f117b463098805d3fc7530a3c65b7e807bbfb5ff
caisenlei <caisenlei@jd.com>
2018-01-08 19:46:07 +0800

    v1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 	res.send('Hello World!项老板'+Date.now());
 })
  
-let server = app.listen(8081, function () {
+let server = app.listen(80, function () {
 	let host = server.address().address;
 	let port = server.address().port;
 	console.log("应用实例，访问地址为 http://%s:%s", host, port);
